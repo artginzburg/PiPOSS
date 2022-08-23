100290f5a9841b2a219f9342c75552a1bccd7e97
Art Ginzburg <ginzburg.arthur@gmail.com>
2022-08-23 23:11:49 +0300

    Initial Commit

---
+++ PiPOSS/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PiPOSS
+//
+//  Created by Arthur Ginzburg on 23.08.2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
